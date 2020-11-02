947e06419a603424a48d373a375b53feed433656
Felipe Lopes <7880548+felops@users.noreply.github.com>
2020-11-01 23:00:36 -0300

    :recycle: use aws_iam_policy_document instead of json

--- lambda_iam.tf
+++ lambda_iam.tf
@@ -1,5 +1,17 @@
-resource "aws_iam_role" "lambda_iam" {
-  name = "lambda_iam"
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
 
-  assume_role_policy = file("lambda_assume_role_policy.json")
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_iam" {
+  name               = "lambda_iam"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
